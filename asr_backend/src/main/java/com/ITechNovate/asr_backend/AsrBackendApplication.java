341e25754f9a3bcaeb9fe18dfbf76839f16e2d12
FranciscoSanchezz <A01656793@tec.mx>
2024-10-07 19:30:22 -0600

    Inicio de Backend del proyecto

---
+++ asr_backend/src/main/java/com/ITechNovate/asr_backend/AsrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ITechNovate.asr_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsrBackendApplication.class, args);
+	}
+
+}
---
+++ asr_backend/src/test/java/com/ITechNovate/asr_backend/AsrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ITechNovate.asr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
