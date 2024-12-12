6d6de7f24d9148060d003be84cf0f716ddb8e902
JourneyHM <A01662556@tec.mx>
2024-12-12 01:26:43 -0600

    Cambios finales

--- asr_backend/src/main/java/com/ITechNovate/asr_backend/config/SecurityConfig.java
+++ asr_backend/src/main/java/com/ITechNovate/asr_backend/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
         http
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers(AUTH_WHITELIST).permitAll()
-                        .requestMatchers("/ws/**").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
